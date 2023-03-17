a416d574e1959eb88df125b6b7654e64317398a9
yh725 <yh725@kakao.com>
2023-03-17 12:47:57 +0900

    검증 2(Bean Validation) - Bean Validation(소개) - Bean Validation(시작) - Bean Validation(프로젝트 준비V3) - Bean Validation(스프링 적용) - Bean Validation(에러 코드) - Bean Validation(오브젝트 오류) - Bean Validation(수정에 적용) - Bean Validation(한계) - Bean Validation(groups)

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,21 +1,37 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
 public class Item {
 
-    private Long id;
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
+	@NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+	private Long id;
 
-    public Item() {
-    }
+	@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+	private String itemName;
 
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+	private Integer price;
+
+	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	@Max(value = 9999, groups = SaveCheck.class) //수정 요구사항 추가
+	private Integer quantity;
+
+	public Item() {
+	}
+
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
 }
